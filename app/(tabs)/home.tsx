bc0ff49b5752e2e62735091a571980041df49746
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-07-01 03:33:01 +0100

    feat: Routing app, Authentication method, Header Component

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '@/components/Home/Header'
+
+export default function home() {
+  return (
+    <View>
+      <Header />
+
+      {/* Slider */}
+
+      {/* Category */}
+
+      {/* Popular Business List */}
+    </View>
+  )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,61 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import LoginSrcreen from "@/components/LoginSrcreen";
+import { 
+  ClerkProvider, 
+  SignedIn, 
+  SignedOut 
+} from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import * as SecureStore from 'expo-secure-store';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+  useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf')
+  })
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <ClerkProvider 
+      tokenCache={tokenCache}
+      publishableKey={
+        process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+      }
+    >
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false}}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginSrcreen />
+      </SignedOut>
+    </ClerkProvider> 
   );
 }
